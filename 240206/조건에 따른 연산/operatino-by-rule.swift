5f20459ea7a202b89706f88d9ddd93d7ba94c5cb
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-02-06 04:52:25 +0900

    Title: 조건에 따른 연산, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/조건에 따른 연산/operatino-by-rule.swift
@@ -0,0 +1,16 @@
+var n = Int(readLine()!)!
+var cnt = 0
+
+while n < 1000 {
+    if n % 2 == 0 {
+        n *= 3
+        n += 1
+    } else {
+        n *= 2
+        n += 2
+    }
+
+    cnt += 1
+}
+
+print(cnt)
