1614585a85c18529dc369e08ed9dcfc9efe617f0
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-02-09 18:59:48 +0900

    Title: 그래프 탐색, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240209/그래프 탐색/graph-traversal.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n)]
+
+for _ in range(m):
+    dep, arr = map(int, input().split())
+    graph[dep-1].append(arr-1)
+    graph[arr-1].append(dep-1)
+
+visited = [False for _ in range(n)]
+
+stack = []
+stack.append(0)
+result = 0
+
+while stack:
+    node = stack.pop()
+    visited[node] = True
+    if node > 0:
+        result += 1
+    
+    for child in graph[node]:
+        if not visited[child]:
+            stack.append(child)
+
+print(result)
