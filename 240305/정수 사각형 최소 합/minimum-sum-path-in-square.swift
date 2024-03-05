26da32c9795426671deddd6341dda0d829209355
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-03-05 18:05:02 +0900

    Title: 정수 사각형 최소 합, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240305/정수 사각형 최소 합/minimum-sum-path-in-square.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let number = Int(readLine()!)!
+
+var grid = [[Int]]()
+
+for _ in 0..<number {
+    let row = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+    grid.append(row)
+}
+
+var dpGrid = Array(repeating: Array(repeating: 0, count: number), count: number)
+
+dpGrid[0][number - 1] = grid[0][number - 1]
+
+for i in (0..<number - 1).reversed() {
+    dpGrid[0][i] = grid[0][i] + dpGrid[0][i+1]
+}
+
+for j in 1..<number {
+    dpGrid[j][number - 1] = grid[j][number - 1] + dpGrid[j - 1][number - 1]
+}
+
+for k in 1..<number {
+    for l in (0..<number - 1).reversed() {
+        dpGrid[k][l] = grid[k][l] + min(dpGrid[k-1][l], dpGrid[k][l+1])
+    }
+}
+
+print(dpGrid[number - 1].min()!)
