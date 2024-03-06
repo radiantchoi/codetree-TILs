52e92c70588a8e499381346d4cd8d451b42ac12c
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-03-06 15:42:55 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240306/정수 사각형 최솟값의 최대/maximin-path-in-square.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var grid = [[Int]]()
+
+for _ in 0..<n {
+    let row = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+    grid.append(row)
+}
+
+var minGrid = Array(repeating: Array(repeating: 0, count: n), count: n)
+minGrid[0][0] = grid[0][0]
+
+for i in 1..<n {
+    minGrid[i][0] = min(grid[i][0], minGrid[i-1][0])
+}
+
+for j in 1..<n {
+    minGrid[0][j] = min(grid[0][j], minGrid[0][j-1])
+}
+
+for i in 1..<n {
+    for j in 1..<n {
+        minGrid[i][j] = min(grid[i][j], max(grid[i-1][j], grid[i][j-1]))
+    }
+}
+
+print(minGrid[n - 1][n - 1])
