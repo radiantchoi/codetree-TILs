af058feee5bd2e61aa4893ad1107adce83f52864
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-03-11 19:20:48 +0900

    Title: 두 수의 합, Time: 1391ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240311/두 수의 합/sum-of-two-num.py
+++ 240311/두 수의 합/sum-of-two-num.py
@@ -4,5 +4,5 @@ n, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 
 cases = list(combinations(numbers, 2))
-possible = list(filter(lambda x: x == k, map(lambda x: x[0] + x[1], cases)))
+possible = list(filter(lambda x: x[0] + x[1] == k, cases))
 print(len(possible))
