bd02a87a607a20990b6e14530e9484f203eaef46
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-03-11 20:03:01 +0900

    Title: 두 수의 합, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240311/두 수의 합/sum-of-two-num.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let orders = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+let numbers = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+
+var storage = [Int: Int]()
+
+for number in numbers {
+    // 숫자의 등장 횟수만 일단 다 기록한다
+    // 각각의 숫자에 대해 대응하는 순서쌍이 있다면 곱한다
+    if storage[number] != nil {
+        storage[number]! += 1
+    } else {
+        storage[number] = 1
+    }
+}
+
+var result = 0
+
+for number in storage.keys {
+    let opposite = orders[1] - number
+    if storage[opposite] != nil {
+        let cases = storage[opposite]! * storage[number]!
+        result += cases
+        storage[opposite] = 0
+        storage[number] = 0
+    }
+}
+
+print(result)
